6a50436e0af4cae9f0e9751af6088f4026840e3f
Aleks Clark <aleks.clark@gmail.com>
2024-05-17 05:00:24 -0500

    wee

--- agent_setup.sh
+++ agent_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -yy iperf net-tools tmux
-git clone https://github.com/aleksclark/bug-free-computing-machine.git tools
-cd tools
+sudo apt-get install -yy iperf net-tools tmux nano
+sudo mkdir tools2
+sudo chown ssm-user tools2
+git clone https://github.com/aleksclark/bug-free-computing-machine.git tools2
+cd tools2
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/aleksclark/bug-free-computing-machine/master/agent_setup.sh)"
+
