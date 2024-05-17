c375a99a1f05697afd5b31004390f1f8acd5ce69
Aleks Clark <aleks.clark@gmail.com>
2024-05-17 05:06:54 -0500

    wee2

--- agent_setup.sh
+++ agent_setup.sh
@@ -5,9 +5,7 @@ sudo apt-get install -yy iperf net-tools tmux nano
 sudo mkdir tools
 sudo chown ssm-user tools
 git clone https://github.com/aleksclark/bug-free-computing-machine.git tools
-sudo cp  tools/mint-agent.service /etc/systemd/system/mint-agent.service
-sudo systemctl daemon-reload
-sudo systemctl stop mint-agent
+cd tools && bash post_setup.sh
 
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/aleksclark/bug-free-computing-machine/master/agent_setup.sh)"
 
---
+++ post_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cp  mint-agent.service /etc/systemd/system/mint-agent.service
+sudo systemctl daemon-reload
+sudo systemctl stop mint-agent
