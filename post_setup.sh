77da25d9b7c3aa646aa0dfa4dd9f3ded9d047f1a
Aleks Clark <aleks.clark@gmail.com>
2024-05-18 06:28:22 -0500

    derp

--- post_setup.sh
+++ post_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo cp  mint-agent.service /etc/systemd/system/mint-agent.service
-sudo systemctl daemon-reload
-sudo systemctl stop mint-agent
+#sudo cp  mint-agent.service /etc/systemd/system/mint-agent.service
+#sudo systemctl daemon-reload
+#sudo systemctl stop mint-agent
+
+
